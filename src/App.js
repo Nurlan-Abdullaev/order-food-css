2c142f671ce3febf14a72f25635c1d8715b43f39
NurlanAbdullaev <nurlan2001002@gmail.com>
2023-07-31 17:52:25 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Header } from './components/header/Header'
+import { MealsSummary } from './components/meals-summary/MealsSummary'
+import { Meals } from './components/meals/Meals'
+
+function App() {
+	return (
+		<>
+			<Header />
+			<MealsSummary />
+			<Meals />
+		</>
+	)
+}
+
+export default App
